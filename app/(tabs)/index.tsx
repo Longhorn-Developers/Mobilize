2b4f60d01513d8e0eb8e92565fdc0f7fe18de8f8
Astrol99 <dngames2006@gmail.com>
2025-06-30 12:16:08 -0700

    feat: sql seed avoidance area data and basic tab layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
-import { Stack, Link } from "expo-router";
+import { Stack } from "expo-router";
 
-import { Button } from "~/components/Button";
 import { Container } from "~/components/Container";
 import { ScreenContent } from "~/components/ScreenContent";
 
@@ -10,9 +9,6 @@ export default function Home() {
       <Stack.Screen options={{ title: "Home" }} />
       <Container>
         <ScreenContent path="app/index.tsx" title="Home"></ScreenContent>
-        <Link href={{ pathname: "/details", params: { name: "Dan" } }} asChild>
-          <Button title="Show Details" />
-        </Link>
       </Container>
     </>
   );
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
-import "../global.css";
+import "~/global.css";
 
 import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }

28b20fe3b9d32678483146c93420afe0a48d98d4
Astrol99 <dngames2006@gmail.com>
2025-06-30 13:59:33 -0700

    feat: added avoidance areas on map

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,11 @@
 import { Stack } from "expo-router";
 import { useEffect } from "react";
-import { Text } from "react-native";
+import MapView, { Polygon } from "react-native-maps";
 
-import { Container } from "~/components/Container";
-import { ScreenContent } from "~/components/ScreenContent";
 import { useAvoidanceStore } from "~/store/avoidanceStore";
 
 export default function Home() {
-  const { avoidanceAreas, avoidanceAreasLoading, fetchAvoidanceAreas } =
-    useAvoidanceStore();
+  const { avoidanceAreas, fetchAvoidanceAreas } = useAvoidanceStore();
 
   useEffect(() => {
     fetchAvoidanceAreas();
@@ -16,27 +13,37 @@ export default function Home() {
 
   return (
     <>
-      <Stack.Screen options={{ title: "Home" }} />
-      <Container>
-        <ScreenContent title="Home">
-          <Text>
-            {avoidanceAreasLoading
-              ? "Fetching avoidance areas..."
-              : `Fetched ${avoidanceAreas.length} avoidance areas.`}
-          </Text>
+      <Stack.Screen options={{ headerShown: false }} />
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          // Default coordinates for UT Tower
+          // longitude: -97.73921,
+          // latitude: 30.28565,
 
-          <Text>
-            {avoidanceAreas.map((area) => (
-                <Text key={area.id}>
-                {area.name}:
-                {area.boundary
-                  ? JSON.stringify(area.boundary)
-                  : "No coordinates"}
-                </Text>
-            ))}
-          </Text>
-        </ScreenContent>
-      </Container>
+          // Coordinates for testing seed avoidance area
+          longitude: -97.7333,
+          latitude: 30.2672,
+
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        {avoidanceAreas.map((area) => (
+          <Polygon
+            key={area.id}
+            coordinates={
+              area.boundary?.coordinates[0].map(([longitude, latitude]) => ({
+                latitude,
+                longitude,
+              })) || []
+            }
+            strokeColor="rgba(255, 0, 0, 0.5)"
+            fillColor="rgba(255, 0, 0, 0.2)"
+            strokeWidth={2}
+          />
+        ))}
+      </MapView>
     </>
   );
 }

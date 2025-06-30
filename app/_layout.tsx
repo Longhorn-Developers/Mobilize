1c7915c6e3adfbb2ae17cb32d80faa88b0463661
Astrol99 <dngames2006@gmail.com>
2025-06-30 16:27:13 -0700

    feat: added react query and refetch hooks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,31 @@
 import "~/global.css";
-
+import { AppStateStatus, Platform } from "react-native";
 import { Stack } from "expo-router";
+import {
+  QueryClient,
+  QueryClientProvider,
+  focusManager,
+} from "@tanstack/react-query";
+import { useAppState } from "~/hooks/useAppState";
+import { useOnlineManager } from "~/hooks/useOnlineManager";
+
+function onAppStateChange(status: AppStateStatus) {
+  // React Query already supports in web browser refetch on window focus by default
+  if (Platform.OS !== "web") {
+    focusManager.setFocused(status === "active");
+  }
+}
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  // react-query refetch on network reconnect
+  useOnlineManager();
+
+  // react-query refetch on app focus
+  useAppState(onAppStateChange);
+
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </QueryClientProvider>
+  );
 }

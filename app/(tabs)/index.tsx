e51e3552c4b1a9976da436f26c317cf61a860f36
Astrol99 <dngames2006@gmail.com>
2025-07-17 18:09:39 -0700

    feat: added report mode and modal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,43 @@
 import { Stack } from "expo-router";
-import { useEffect } from "react";
-import MapView, { Polygon } from "react-native-maps";
+import { useState } from "react";
+import MapView, {
+  Polygon,
+  Marker,
+  type LatLng,
+  type MapPressEvent,
+} from "react-native-maps";
 import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
 import { supabase } from "~/utils/supabase";
+import { Button } from "~/components/Button";
+import { ReportModal } from "~/components/ReportModal";
 
 export default function Home() {
+  const [isReportMode, setIsReportMode] = useState(false);
+  const [aaPoints, setAAPoints] = useState<LatLng[]>([]);
+
   const { data: avoidanceAreas } = useQuery(
     supabase.from("avoidance_areas_with_geojson").select("id,name,boundary"),
   );
 
-  useEffect(() => {
-    console.log(
-      "Fetched avoidance areas:",
-      avoidanceAreas?.map((area) => area.name),
-    );
-  }, [avoidanceAreas]);
+  // Add pressed coordinates to marked points
+  const handleMapPress = (event: MapPressEvent) => {
+    if (
+      !isReportMode ||
+      !event.nativeEvent.coordinate ||
+      aaPoints.includes(event.nativeEvent.coordinate)
+    )
+      return;
+
+    event.persist();
+    setAAPoints((prev) => [...(prev || []), event.nativeEvent.coordinate]);
+  };
 
   return (
     <>
       <Stack.Screen options={{ title: "Home", headerShown: false }} />
       <MapView
         style={{ flex: 1 }}
+        onPress={handleMapPress}
         initialRegion={{
           // Default coordinates for UT Tower
           // longitude: -97.73921,
@@ -34,6 +51,7 @@ export default function Home() {
           longitudeDelta: 0.01,
         }}
       >
+        {/* Show avoidance area polygons */}
         {avoidanceAreas &&
           avoidanceAreas.map((area) => (
             <Polygon
@@ -49,7 +67,43 @@ export default function Home() {
               strokeWidth={2}
             />
           ))}
+
+        {/* Show selected avoidance area coordinate points */}
+        {aaPoints.map((point) => (
+          <Marker
+            key={`${point.latitude}-${point.longitude}`}
+            coordinate={{
+              latitude: point.latitude,
+              longitude: point.longitude,
+            }}
+            pinColor="red"
+          />
+        ))}
       </MapView>
+
+      {/* Bottom right button to enter report mode */}
+      <Button
+        className="absolute bottom-4 right-4"
+        title={isReportMode ? "Exit Report Mode" : "Report Area"}
+        onPress={() => setIsReportMode(!isReportMode)}
+      />
+
+      {/* Report Mode Dialog */}
+      <ReportModal
+        className="absolute left-12 right-12 top-20"
+        isVisible={isReportMode}
+        aaPoints={aaPoints}
+        onCancel={() => {
+          setIsReportMode(false);
+          setAAPoints([]);
+        }}
+        onSubmit={() => {
+          console.log("Submitting report with points:", aaPoints);
+          setIsReportMode(false);
+          setAAPoints([]);
+        }}
+        onClearPoints={() => setAAPoints([])}
+      />
     </>
   );
 }
---
+++ components/ReportModal.tsx
@@ -0,0 +1,55 @@
+import { View, Text } from "react-native";
+import { Button } from "./Button";
+import { type LatLng } from "react-native-maps";
+
+interface ReportModeDialogProps {
+  className?: string;
+  isVisible: boolean;
+  aaPoints: LatLng[];
+  onCancel: () => void;
+  onSubmit: () => void;
+  onClearPoints: () => void;
+}
+
+export function ReportModal({
+  className,
+  isVisible,
+  aaPoints,
+  onCancel,
+  onSubmit,
+  onClearPoints,
+}: ReportModeDialogProps) {
+  if (!isVisible) return null;
+
+  return (
+    <>
+      {/* Main Modal */}
+      <View
+        className={`rounded-lg border border-gray-200 bg-white p-4 shadow-lg ${className}`}
+      >
+        {/* Heading and subheading */}
+        <Text className="mb-2 text-4xl font-bold">Avoidance Area</Text>
+        <Text className="mb-4 text-xl font-semibold">
+          Report a temporary blockage
+        </Text>
+
+        {/* Main content */}
+        <Text className="mb-4 text-gray-600">Step 1 of 3</Text>
+        <Text className="mb-4 font-semibold">
+          Please indicate the problem area by marking five points on the map.
+        </Text>
+
+        <Button title="Next" onPress={onSubmit} />
+      </View>
+
+      {/* Clear Points Button */}
+      {aaPoints.length > 0 && (
+        <Button
+          className="absolute bottom-4 left-4"
+          title="Clear Points"
+          onPress={onClearPoints}
+        />
+      )}
+    </>
+  );
+}

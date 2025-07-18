b62063a83db1c4343da2877c6c01cba2fb01b927
Astrol99 <dngames2006@gmail.com>
2025-07-17 18:43:52 -0700

    feat: added disabled variant to button

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,24 +1,41 @@
-import { forwardRef } from 'react';
-import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
+import { forwardRef } from "react";
+import {
+  Text,
+  TouchableOpacity,
+  TouchableOpacityProps,
+  View,
+} from "react-native";
 
 type ButtonProps = {
   title: string;
+  variant?: "primary" | "disabled";
 } & TouchableOpacityProps;
 
-export const Button = forwardRef<View, ButtonProps>(({ title, ...touchableProps }, ref) => {
-  return (
-    <TouchableOpacity
-      ref={ref}
-      {...touchableProps}
-      className={`${styles.button} ${touchableProps.className}`}>
-      <Text className={styles.buttonText}>{title}</Text>
-    </TouchableOpacity>
-  );
-});
+export const Button = forwardRef<View, ButtonProps>(
+  ({ title, variant = "primary", ...touchableProps }, ref) => {
+    return (
+      <TouchableOpacity
+        ref={ref}
+        {...touchableProps}
+        className={`${styles.button} ${variant === "disabled" ? styles.disabledButton : styles.primaryButton} ${touchableProps.className}`}
+      >
+        <Text
+          className={`${styles.buttonText} ${variant === "disabled" ? styles.disabledButtonText : styles.primaryButtonText}`}
+        >
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  },
+);
 
-Button.displayName = 'Button';
+Button.displayName = "Button";
 
 const styles = {
-  button: 'items-center bg-ut-burntorange rounded-lg shadow-md p-4',
-  buttonText: 'text-white text-lg font-semibold text-center',
+  button: "items-center rounded-lg shadow-md p-4",
+  primaryButton: "bg-ut-burntorange",
+  disabledButton: "bg-theme-majorgridline",
+  buttonText: "text-lg font-semibold text-center",
+  primaryButtonText: "text-white",
+  disabledButtonText: "text-black",
 };

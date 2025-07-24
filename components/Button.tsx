caeb2a5cad132f1be5ce09bf6b7f2c6b0b30fd1b
Astrol99 <dngames2006@gmail.com>
2025-07-23 22:50:01 -0700

    feat: added ghost variant to btn and refact for scalability

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,19 +8,41 @@ import {
 
 type ButtonProps = {
   title: string;
-  variant?: "primary" | "disabled";
+  variant?: "primary" | "disabled" | "ghost";
 } & TouchableOpacityProps;
 
 export const Button = forwardRef<View, ButtonProps>(
   ({ title, variant = "primary", ...touchableProps }, ref) => {
+    const getButtonStyle = () => {
+      switch (variant) {
+        case "disabled":
+          return styles.disabledButton;
+        case "ghost":
+          return styles.ghostButton;
+        default:
+          return styles.primaryButton;
+      }
+    };
+
+    const getTextStyle = () => {
+      switch (variant) {
+        case "disabled":
+          return styles.disabledButtonText;
+        case "ghost":
+          return styles.ghostButtonText;
+        default:
+          return styles.primaryButtonText;
+      }
+    };
+
     return (
       <TouchableOpacity
         ref={ref}
         {...touchableProps}
-        className={`${styles.button} ${variant === "disabled" ? styles.disabledButton : styles.primaryButton} ${touchableProps.className}`}
+        className={`${styles.button} ${getButtonStyle()} ${touchableProps.className}`}
       >
         <Text
-          className={`${styles.buttonText} ${variant === "disabled" ? styles.disabledButtonText : styles.primaryButtonText}`}
+          className={`${styles.buttonText} ${getTextStyle()}`}
         >
           {title}
         </Text>
@@ -35,7 +57,9 @@ const styles = {
   button: "items-center rounded-md shadow-md py-2 px-4",
   primaryButton: "bg-ut-burntorange",
   disabledButton: "bg-ut-black/20",
+  ghostButton: "bg-transparent border border-ut-burntorange",
   buttonText: "text-lg font-semibold text-center",
   primaryButtonText: "text-white",
   disabledButtonText: "text-black",
+  ghostButtonText: "text-ut-burntorange",
 };

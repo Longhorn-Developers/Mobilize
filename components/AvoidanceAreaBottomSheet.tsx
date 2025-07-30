39d2023977b34cd4bb966062e70d31f1aab4373d
Astrol99 <dngames2006@gmail.com>
2025-07-29 18:56:57 -0700

    feat: PoC for bottom sheet

---
+++ components/AvoidanceAreaBottomSheet.tsx
@@ -0,0 +1,45 @@
+import { forwardRef, useMemo } from "react";
+import { Text, View } from "react-native";
+import { BottomSheetModal, BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import colors from "~/types/colors";
+import { WarningIcon } from "phosphor-react-native";
+
+const AvoidanceAreaBottomSheet = forwardRef<BottomSheetModal>(({}, ref) => {
+  // Variables
+  const snapPoints = useMemo(() => ["25%", "50%", "90%"], []);
+
+  return (
+    <BottomSheetModal
+      enablePanDownToClose
+      ref={ref}
+      snapPoints={snapPoints}
+      handleIndicatorStyle={{
+        backgroundColor: colors.theme.majorgridline,
+        width: 80,
+      }}
+    >
+      <BottomSheetScrollView className="flex-1 px-8 py-4">
+        {/* Heading Container */}
+        <View className="flex-row items-center gap-4">
+          {/* Icon Container */}
+          <View className="rounded-lg bg-theme-red/20 p-3">
+            <WarningIcon size={32} color={colors.theme.red} />
+          </View>
+
+          {/* Heading and subheading */}
+          <View>
+            <Text className="text-4xl font-bold">Avoidance Area</Text>
+            <Text className="text-lg font-medium">Temporary Blockage</Text>
+          </View>
+        </View>
+
+        {/* Avoidance Area ID */}
+        <View className="mt-4">
+          <Text className="text-lg font-semibold">Avoidance Area ID:</Text>
+        </View>
+      </BottomSheetScrollView>
+    </BottomSheetModal>
+  );
+});
+
+export default AvoidanceAreaBottomSheet;

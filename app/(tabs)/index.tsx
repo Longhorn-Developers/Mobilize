0673c575951d832fd5202286ec0ba864cbe4b249
Astrol99 <dngames2006@gmail.com>
2025-08-04 13:03:58 -0500

    feat: added PoC for displaying accessiblility POIs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,15 @@
-import { Stack } from "expo-router";
-import { useState, useRef } from "react";
-import { View } from "react-native";
-import { AppleMaps, Coordinates } from "expo-maps";
-import { useImage } from "expo-image";
-import { AppleMapsPolygon } from "expo-maps/build/apple/AppleMaps.types";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { Button } from "~/components/Button";
-import ReportModal from "~/components/ReportModal";
-import AvoidanceAreaBottomSheet from "~/components/AvoidanceAreaBottomSheet";
 import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
+import { useImage } from "expo-image";
+import { AppleMaps, Coordinates } from "expo-maps";
+import { AppleMapsPolygon } from "expo-maps/build/apple/AppleMaps.types";
+import { Stack } from "expo-router";
+import { useRef, useState } from "react";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import AvoidanceAreaBottomSheet from "~/components/AvoidanceAreaBottomSheet";
+import { Button } from "~/components/Button";
+import ReportModal from "~/components/ReportModal";
 import { supabase } from "~/utils/supabase";
 
 export default function Home() {
@@ -21,9 +21,17 @@ export default function Home() {
 
   const bottomSheetRef = useRef<BottomSheetModal>(null);
 
-  const pointImage = useImage(require("../../assets/point.svg"), {
-    maxWidth: 32,
-    maxHeight: 32,
+  const pointIcon = useImage(require("../../assets/point.svg"), {
+    maxWidth: 64,
+    maxHeight: 64,
+    onError(error) {
+      console.error(error);
+    },
+  });
+
+  const autoDoorIcon = useImage(require("../../assets/autodoor.svg"), {
+    maxWidth: 64,
+    maxHeight: 64,
     onError(error) {
       console.error(error);
     },
@@ -33,12 +41,37 @@ export default function Home() {
     supabase.from("avoidance_areas_with_geojson").select("id,boundary"),
   );
 
+  const POIs = [
+    {
+      coordinates: [
+        {
+          latitude: 30.26523897879868,
+          longitude: -97.73236982430406,
+        },
+      ],
+    },
+  ];
+
+  // Check if map pressed is among one of the POIs
+  const handlePOIPress = (event: any) => {
+    const CLICK_TOLERANCE = 0.0001;
+    const POIclicked = POIs.find((poi) => {
+      const latDiff = Math.abs(poi.coordinates[0].latitude - event.latitude);
+      const lonDiff = Math.abs(poi.coordinates[0].longitude - event.longitude);
+      return latDiff <= CLICK_TOLERANCE && lonDiff <= CLICK_TOLERANCE;
+    });
+    if (POIclicked) {
+      console.log("POI CLICKED");
+    }
+  };
+
   const handleMapPress = (event: any) => {
     if (isReportMode) {
       if (reportStep !== 0) return;
       // Add pressed coordinates to marked points
       setAAPointsReport((prev) => [...prev, event]);
     } else {
+      handlePOIPress(event);
       bottomSheetRef.current?.close();
     }
   };
@@ -58,8 +91,20 @@ export default function Home() {
 
       <AppleMaps.View
         style={{ flex: 1 }}
-        onMapClick={handleMapPress}
         onPolygonClick={handleAvoidanceAreaPress}
+        onMapClick={handleMapPress}
+        cameraPosition={{
+          coordinates: {
+            // Default coordinates for UT Tower
+            // longitude: -97.73921,
+            // latitude: 30.28565,
+
+            // Testing camera position for test avoidance area
+            longitude: -97.7333,
+            latitude: 30.2672,
+          },
+          zoom: 16,
+        }}
         polygons={[
           // Avoidance areas from the database
           ...(avoidanceAreas || []).map<AppleMapsPolygon>((area) => ({
@@ -81,22 +126,20 @@ export default function Home() {
             lineWidth: 2,
           },
         ]}
-        annotations={aaPointsReport.map((point) => ({
-          coordinates: point,
-          icon: pointImage ? pointImage : undefined,
-        }))}
-        cameraPosition={{
-          coordinates: {
-            // Default coordinates for UT Tower
-            // longitude: -97.73921,
-            // latitude: 30.28565,
-
-            // Testing camera position for test avoidance area
-            longitude: -97.7333,
-            latitude: 30.2672,
-          },
-          zoom: 16,
-        }}
+        annotations={[
+          // User selected aaPoints to report
+          ...aaPointsReport.map((point) => ({
+            coordinates: point,
+            icon: pointIcon ? pointIcon : undefined,
+          })),
+          // Points of Interest (POIs)
+          ...POIs.flatMap((poi) =>
+            poi.coordinates.map((coord) => ({
+              coordinates: coord,
+              icon: autoDoorIcon ? autoDoorIcon : undefined,
+            })),
+          ),
+        ]}
       />
 
       {isReportMode ? (

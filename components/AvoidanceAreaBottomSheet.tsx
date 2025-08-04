90b49447cd4e3970372d38cb421c66d1082dfbb3
Astrol99 <dngames2006@gmail.com>
2025-08-03 19:53:26 -0500

    feat: connected aa bottom sheet to backend and split components

--- components/AvoidanceAreaBottomSheet.tsx
+++ components/AvoidanceAreaBottomSheet.tsx
@@ -1,10 +1,9 @@
 import { ForwardedRef } from "react";
-import { Text, View } from "react-native";
-import { BottomSheetModal, BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { BottomSheetModal } from "@gorhom/bottom-sheet";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import colors from "~/types/colors";
-import { WarningIcon } from "phosphor-react-native";
 import { AppleMapsPolygon } from "expo-maps/build/apple/AppleMaps.types";
+import AvoidanceAreaDetails from "./AvoidanceAreaDetails";
 
 interface AvoidanceAreaBottomSheetProps {
   ref: ForwardedRef<BottomSheetModal>;
@@ -24,29 +23,8 @@ const AvoidanceAreaBottomSheet = ({ ref }: AvoidanceAreaBottomSheetProps) => {
       bottomInset={bottomTabBarHeight}
     >
       {({ data }) => {
-        console.log(data?.id);
-        return (
-          <BottomSheetScrollView className="flex-1 px-8 py-4">
-            {/* Heading Container */}
-            <View className="flex-row items-center gap-4">
-              {/* Icon Container */}
-              <View className="rounded-lg bg-theme-red/20 p-3">
-                <WarningIcon size={32} color={colors.theme.red} />
-              </View>
-
-              {/* Heading and subheading */}
-              <View>
-                <Text className="text-4xl font-bold">Avoidance Area</Text>
-                <Text className="text-lg font-medium">Temporary Blockage</Text>
-              </View>
-            </View>
-
-            {/* Avoidance Area ID */}
-            <View className="mt-4">
-              <Text className="text-lg font-semibold">Avoidance Area ID:</Text>
-            </View>
-          </BottomSheetScrollView>
-        );
+        if (!data || !data.id) return;
+        return <AvoidanceAreaDetails areaId={data.id} />;
       }}
     </BottomSheetModal>
   );

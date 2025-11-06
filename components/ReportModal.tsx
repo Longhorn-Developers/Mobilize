01d1ba31217f3c948251066cdf01da5ead7d1c3d
Astrol99 <dngames2006@gmail.com>
2025-11-06 17:56:38 -0600

    refact: streamline app configuration and update map handling to use react-native-maps

--- components/ReportModal.tsx
+++ components/ReportModal.tsx
@@ -20,15 +20,15 @@ import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z, ZodType } from "zod";
 import Toast from "react-native-toast-message";
-import { Coordinates } from "expo-maps";
+import { LatLng } from "react-native-maps";
 
 const reportFormSchema = z.object({
   aaPoints: z
     .array(
       z.object({
-        latitude: z.number().optional(),
-        longitude: z.number().optional(),
-      }) satisfies ZodType<Coordinates>,
+        latitude: z.number(),
+        longitude: z.number(),
+      }) satisfies ZodType<LatLng>,
     )
     .min(
       3,
@@ -50,9 +50,9 @@ type ReportFormData = z.infer<typeof reportFormSchema>;
 interface ReportModeDialogProps {
   className?: string;
   style?: ViewStyle;
-  aaPoints: Coordinates[];
+  aaPoints: LatLng[];
   currentStep: number;
-  setAAPoints: (points: Coordinates[]) => void;
+  setAAPoints: (points: LatLng[]) => void;
   setCurrentStep: (index: number) => void;
   onSubmit: (data: ReportFormData) => void;
   onExit: () => void;
--- global.d.ts
+++ global.d.ts
@@ -1,11 +1,3 @@
-import type { SvgProps as DefaultSvgProps } from "react-native-svg";
-
-declare module "react-native-svg" {
-  interface SvgProps extends DefaultSvgProps {
-    className?: string;
-  }
-}
-
 declare module "phosphor-react-native" {
   interface IconProps extends DefaultIconProps {
     className?: string;

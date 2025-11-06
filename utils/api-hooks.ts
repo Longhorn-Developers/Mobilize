eb7eaefd47defcab668b7b22036ee705c518ecba
Astrol99 <dngames2006@gmail.com>
2025-11-06 11:55:52 -0600

    fix: update compatibility date in wrangler configuration and adjust stale time for POIs query to 24 hours

--- utils/api-hooks.ts
+++ utils/api-hooks.ts
@@ -21,7 +21,7 @@ export function usePOIs() {
   return useQuery({
     queryKey: queryKeys.pois,
     queryFn: () => apiClient.getPOIs(),
-    staleTime: 1000 * 60 * 60, // 1 hour
+    staleTime: 1000 * 60 * 60 * 24, // 24 hour
   });
 }
 
